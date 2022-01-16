2934264b92d62ceb28971fc1fe4383944f525150
Grzegorz Kocot <grzegorz.kocot@gmail.com>
2022-01-16 17:47:39 +0100

    Moved webpage sources, building webpage to www/ folder so it is easier copying to SD

---
+++ web/src/views/WiFi.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>WiFi</div>
+    <button @click="getConfig()">Get Config</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    getConfig() {
+      this.$ajax
+        .get("/api/v1/config")
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    this.getConfig();
+  },
+};
+</script>
