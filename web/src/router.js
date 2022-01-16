730d0e64db134aee6d10b2413e9b3a3a090bc45a
Grzegorz Kocot <grzegorz.kocot@gmail.com>
2022-01-16 22:11:31 +0100

    Routing updates

--- web/src/router.js
+++ web/src/router.js
@@ -9,10 +9,15 @@ import IO from "./views/IO.vue";
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
+      redirect: "/home",
+    },
+    {
+      path: "/home",
       name: "home",
       component: Home,
     },
@@ -35,6 +40,10 @@ export default new Router({
       path: "/io",
       name: "io",
       component: IO,
+    },
+    {
+      path: "*",
+      redirect: "/home",
     },
   ],
 });
