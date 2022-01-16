ecd40ba80c05ede8f06eca79320252ec921fa3e5
Grzegorz Kocot <grzegorz.kocot@gmail.com>
2022-01-16 20:15:50 +0100

    WiFi view form

--- web/src/views/WiFi.vue
+++ web/src/views/WiFi.vue
@@ -1,14 +1,32 @@
 <template>
-  <div>
-    <div>WiFi</div>
-    <button @click="getConfig()">Get Config</button>
-  </div>
+  <v-container>
+    <v-select label="Mode" v-model="wifi_mode" :items="wifi_modes"></v-select>
+    <v-text-field label="SSID" v-model="wifi_ssid"></v-text-field>
+    <v-text-field
+      label="Password"
+      v-model="wifi_password"
+      :type="wifi_password_visible ? 'text' : 'password'"
+      :append-icon="wifiPasswordIcon()"
+      @click:append="() => (wifi_password_visible = !wifi_password_visible)"
+    ></v-text-field>
+  </v-container>
 </template>
 
 <script>
+import { mdiEye, mdiEyeOff } from "@mdi/js";
+
 export default {
   data() {
-    return {};
+    console.log(mdiEye);
+    console.log(mdiEyeOff);
+    return {
+      wifi_modes: ["Access Point", "Station"],
+      wifi_mode: "Access Point",
+      wifi_ssid: "myssid",
+      wifi_password: "mypassword",
+      wifi_password_visible: false,
+      wifi_password_icon: mdiEye,
+    };
   },
   methods: {
     getConfig() {
@@ -20,6 +38,9 @@ export default {
         .catch((error) => {
           console.log(error);
         });
+    },
+    wifiPasswordIcon() {
+      return this.wifi_password_visible ? mdiEyeOff : mdiEye;
     },
   },
   mounted() {
